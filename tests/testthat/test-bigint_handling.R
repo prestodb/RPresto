043f172ed6fc490379f2e8f7627f3bbd9d5d45ec
Ismail Onur Filiz <onur@fb.com>
2016-03-13 16:48:34 -0700

    Add warning for integer columns being corrupted by lacking 64-bit integer support
    Presto BIGINT columns support 64-bit integers, whereas R is still
    lacking full support for them. R silently casts any such value to a
    double, which leads to loss of precision and is impossible to recover
    afterwards.
    
    jsonlite solves this by utilizing the bit64 package. That approach has
    lots of caveats though, so we need to investigate more to see if we can
    do the same.

---
+++ tests/testthat/test-bigint_handling.R
@@ -0,0 +1,28 @@
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+context('bigint handling')
+
+source('utilities.R')
+
+test_that('Non 32-bit integers give warning', {
+  conn <- setup_live_connection()
+
+  expect_warning(
+    dbGetQuery(conn, "SELECT 2147483648 AS a, 3 AS b"),
+    'columns \\[1\\] are cast to double'
+  )
+  expect_warning(
+    dbGetQuery(conn, "SELECT -3 AS d, -2147483649 AS c"),
+    'columns \\[2\\] are cast to double'
+  )
+  rv <- suppressWarnings(dbGetQuery(conn, "SELECT 9223372036854775807 AS number"))
+  expect_false(as.character(rv[['number']]) == "9223372036854775807")
+
+  rv <- dbGetQuery(conn, "SELECT CAST(9223372036854775807 AS VARCHAR) AS string")
+  expect_equal(rv[['string']], "9223372036854775807")
+})
