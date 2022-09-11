21557cdafa53de93f68928cdc9c952fe7eddf005
Jarod G.R. Meng <jarodm@fb.com>
2022-09-11 10:30:33 +0800

    Move table overwriting from dbWriteTable upstream to dbCreateTableAs

--- R/dbCreateTableAs.R
+++ R/dbCreateTableAs.R
@@ -11,18 +11,23 @@ NULL
 #'
 #' @inheritParams DBI::dbReadTable
 #' @inheritParams sqlCreateTableAs
+#' @param sql a character string containing SQL statement.
+#' @param overwrite A boolean indicating if an existing table should be
+#'   overwritten. Default to FALSE.
 #' @export
 setGeneric("dbCreateTableAs",
-  def = function(conn, name, sql, with = NULL, ...) {
+  def = function(conn, name, sql, overwrite = FALSE, with = NULL, ...) {
     standardGeneric("dbCreateTableAs")
   }
 )
 
 #' @rdname PrestoConnection-class
-#' @param sql a character string containing SQL statement.
 #' @usage NULL
-.dbCreateTableAs <- function(conn, name, sql, with = NULL, ...) {
+.dbCreateTableAs <- function(
+  conn, name, sql, overwrite = FALSE, with = NULL, ...
+) {
   stopifnot(is.character(sql), length(sql) == 1)
+  stopifnot(length(overwrite) == 1, is.logical(overwrite), !is.na(overwrite))
 
   query <- sqlCreateTableAs(
     con = conn,
@@ -31,7 +36,47 @@ setGeneric("dbCreateTableAs",
     with = with,
     ...
   )
-  DBI::dbExecute(conn, query)
+  if (dbExistsTable(conn, name)) {
+    if (identical(overwrite, TRUE)) {
+      # Rename the existing table to a temporary random name
+      # Create the table using the name
+      # Remove the existing table and let the user know that the table is
+      # overwritten
+      rn <- paste0(
+        'temp_', paste(sample(letters, 10, replace = TRUE), collapse = '')
+      )
+      DBI::dbExecute(
+        conn,
+        dbplyr::sql(paste('ALTER TABLE', name, 'RENAME TO', rn))
+      )
+      tryCatch(
+        {
+          DBI::dbExecute(conn, query)
+        },
+        error = function(e) {
+          # In case of error, revert the original table's name
+          DBI::dbExecute(
+            conn,
+            dbplyr::sql(paste('ALTER TABLE', rn, 'RENAME TO', name))
+          )
+          stop(
+            'Overwriting table ', name, ' failed with error: "',
+            conditionMessage(e), '".', call. = FALSE
+          )
+        }
+      )
+      if (dbRemoveTable(conn, rn)) {
+        message('The table ', name, ' is overwritten.')
+      }
+    } else {
+      stop(
+        'The table ', name, ' exists but overwrite is set to FALSE.',
+        call. = FALSE
+      )
+    }
+  } else {
+    DBI::dbExecute(conn, query)
+  }
   invisible(TRUE)
 }
 
