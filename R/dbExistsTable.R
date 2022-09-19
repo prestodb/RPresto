9aee2848e39fba452a136b6eab0bba837552459c
Jarod G.R. Meng <jarodm@fb.com>
2022-09-19 15:11:08 +0800

    Use information_schema.columns to implement dbExistsTable

--- R/dbExistsTable.R
+++ R/dbExistsTable.R
@@ -14,19 +14,21 @@ setMethod(
   "dbExistsTable",
   c("PrestoConnection", "character"),
   function(conn, name, ...) {
-    table_name <- DBI::dbQuoteIdentifier(conn, name)
-    tryCatch(
-      {
-        res <- dplyr::db_query_fields(conn, table_name)
-        return(TRUE)
-      },
-      error = function(e) {
-        if (grepl("Table .* does not exist", conditionMessage(e))) {
-          return(FALSE)
-        } else {
-          stop("Cannot tell if ", name, " exists.", call. = FALSE)
-        }
-      }
+    # This is necessary because name might be a quoted identifier rather than
+    # just a string (see #167)
+    table_id <- DBI::dbQuoteIdentifier(conn, name)
+    table_name <- DBI::dbUnquoteIdentifier(conn, table_id)[[1]]@name
+    res <- DBI::dbGetQuery(
+      conn,
+      paste0("
+        SELECT COUNT(*) AS n
+        FROM information_schema.columns
+        WHERE
+          table_catalog = '", conn@catalog, "' AND
+          table_schema = '", conn@schema, "' AND
+          table_name = '", table_name, "'
+      ")
     )
+    return((res$n > 0))
   }
 )
