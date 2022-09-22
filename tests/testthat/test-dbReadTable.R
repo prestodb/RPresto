2c02d89469c9b1b21cd4eefe4b0f43e13db4d27b
Jarod G.R. Meng <jarodm@fb.com>
2022-09-22 21:30:36 +0800

    Implement dbReadTable() for PrestoConnection so that bigint can be passed

---
+++ tests/testthat/test-dbReadTable.R
@@ -0,0 +1,31 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+context("dbReadTable")
+
+source("utilities.R")
+
+test_that("dbReadTable works with live database", {
+  conn <- setup_live_connection()
+  test_table_name <- "test_dbreadtable"
+  if (dbExistsTable(conn, test_table_name)) {
+    dbRemoveTable(conn, test_table_name)
+  }
+  test_statement <- "SELECT CAST(1 AS BIGINT) AS field1"
+  expect_true(dbCreateTableAs(conn, test_table_name, test_statement))
+  expect_true(dbExistsTable(conn, test_table_name))
+  expect_equal_data_frame(
+    dbReadTable(conn, test_table_name),
+    tibble::tibble(field1 = 1L)
+  )
+  # bigint works
+  expect_equal_data_frame(
+    dbReadTable(conn, test_table_name, bigint = "character"),
+    tibble::tibble(field1 = "1")
+  )
+  expect_true(dbRemoveTable(conn, test_table_name))
+})
+
