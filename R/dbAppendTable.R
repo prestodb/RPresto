d1f37b16e0b7386d6e4d5043c307905e6ba12c89
Jarod G.R. Meng <jarodm@fb.com>
2022-10-30 10:44:50 +0800

    Use chunks to better manage dbAppendTable() upload

--- R/dbAppendTable.R
+++ R/dbAppendTable.R
@@ -15,8 +15,31 @@ NULL
   if (any(is_factor)) {
     value[is_factor] <- lapply(value[is_factor], as.character)
   }
-  sql <- DBI::sqlAppendTable(conn, name, value, row.names = FALSE)
-  DBI::dbExecute(conn, sql)
+  chunks <- (as.integer(object.size(value)) %/% 7.5e5) + 1
+  if (chunks >= 100) {
+    stop("The value to append is too large.", call. = FALSE)
+  }
+  chunk_size <- nrow(value) %/% chunks
+  total_rows <- 0L
+  pb <- progress::progress_bar$new(
+    format = "  appending chunk #:chunk [:bar] :percent in :elapsed",
+    clear = FALSE,
+    total = 100,
+    width = 80,
+    show_after = 1
+  )
+  pb$tick(0, tokens = list(chunk = 0L))
+  for (i in 1:chunks) {
+    start <- (i-1)*chunk_size+1
+    end <- min(i*chunk_size, nrow(value))
+    sql <- DBI::sqlAppendTable(
+      conn, name, dplyr::slice(value, start:end), row.names = FALSE
+    )
+    rows <- DBI::dbExecute(conn, sql, quiet = TRUE)
+    pb$update(ratio = i/chunks, tokens = list(chunk = i))
+    total_rows <- total_rows + rows
+  }
+  return(total_rows)
 }
 
 #' @rdname PrestoConnection-class
