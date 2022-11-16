d708827a15dd4b2df0ead923c7cf471ff5b4454b
Jarod Meng <jarodm@fb.com>
2022-11-16 20:21:27 +0800

    Make sure added chunk field is an integer

--- tests/testthat/test-add_chunk.R
+++ tests/testthat/test-add_chunk.R
@@ -20,21 +20,28 @@ test_that("add_chunk returns the original data frame if within size", {
 })
 
 test_that("add_chunk adds a new field when larger than size limit", {
+  chunk_iris <- add_chunk(iris, chunk_size = 2000)
   expect_equal(
-    colnames(add_chunk(iris, chunk_size = 2000)),
+    colnames(chunk_iris),
     c(colnames(iris), "chunk_idx")
   )
+  expect_equal(class(chunk_iris$chunk_idx), "integer")
+  chunk_iris_2 <-
+    add_chunk(iris, chunk_size = 2000, new_chunk_field_name = "chunk")
   expect_equal(
-    colnames(add_chunk(iris, chunk_size = 2000, new_chunk_field_name = "chunk")),
+    colnames(chunk_iris_2),
     c(colnames(iris), "chunk")
   )
-  new_iris <- add_chunk(iris, chunk_size = 2000, chunk_fields = c("Species"))
+  expect_equal(class(chunk_iris_2$chunk), "integer")
+  chunk_iris_field <-
+    add_chunk(iris, chunk_size = 2000, chunk_fields = c("Species"))
   expect_equal(
-    colnames(new_iris),
+    colnames(chunk_iris_field),
     c(colnames(iris), "chunk_idx")
   )
+  expect_equal(class(chunk_iris_field$chunk_idx), "integer")
   expect_equal(
-    nrow(dplyr::count(new_iris, Species, chunk_idx)),
+    nrow(dplyr::count(chunk_iris_field, Species, chunk_idx)),
     6L
   )
 })
