47eb1713f00af30c1c2ad480e9df5dbe1af7fea6
Jarod G.R. Meng <jarodm@fb.com>
2023-01-13 11:17:58 +0800

    add_chunk() uses VALUES statement to compute the number of rows in each chunk

--- tests/testthat/test-add_chunk.R
+++ tests/testthat/test-add_chunk.R
@@ -14,7 +14,7 @@ test_that("add_chunk returns the original data frame if within size", {
     iris
   )
   expect_equal_data_frame(
-    add_chunk(iris, chunk_fields = c("Species")),
+    add_chunk(iris, base_chunk_fields = c("Species")),
     iris
   )
 })
@@ -23,9 +23,9 @@ test_that("add_chunk adds a new field when larger than size limit", {
   chunk_iris <- add_chunk(iris, chunk_size = 2000)
   expect_equal(
     colnames(chunk_iris),
-    c(colnames(iris), "chunk_idx")
+    c(colnames(iris), "aux_chunk_idx")
   )
-  expect_equal(class(chunk_iris$chunk_idx), "integer")
+  expect_equal(class(chunk_iris$aux_chunk_idx), "integer")
   chunk_iris_2 <-
     add_chunk(iris, chunk_size = 2000, new_chunk_field_name = "chunk")
   expect_equal(
@@ -34,14 +34,14 @@ test_that("add_chunk adds a new field when larger than size limit", {
   )
   expect_equal(class(chunk_iris_2$chunk), "integer")
   chunk_iris_field <-
-    add_chunk(iris, chunk_size = 2000, chunk_fields = c("Species"))
+    add_chunk(iris, chunk_size = 2000, base_chunk_fields = c("Species"))
   expect_equal(
     colnames(chunk_iris_field),
-    c(colnames(iris), "chunk_idx")
+    c(colnames(iris), "aux_chunk_idx")
   )
-  expect_equal(class(chunk_iris_field$chunk_idx), "integer")
+  expect_equal(class(chunk_iris_field$aux_chunk_idx), "integer")
   expect_equal(
-    nrow(dplyr::count(chunk_iris_field, Species, chunk_idx)),
-    6L
+    nrow(dplyr::count(chunk_iris_field, Species, aux_chunk_idx)),
+    9L
   )
 })
