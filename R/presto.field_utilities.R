14f56819eb2f6cf968ab6e33aa505b00168a47ce
Jarod G.R. Meng <genrong.meng@gmail.com>
2024-12-22 23:28:41 +0800

    Update function documentations

--- R/presto.field_utilities.R
+++ R/presto.field_utilities.R
@@ -4,6 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+#' @export
 print.presto.field <- function(x, ..., prefix = "") {
   cat(prefix, "presto.field: \n", sep = "")
   cat(prefix, "..Name: ", x$name_, "\n", sep = "")
