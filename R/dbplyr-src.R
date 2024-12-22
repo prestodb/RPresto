45f90838104d85ea6c5589895353198ac5f1859f
Jarod G.R. Meng <genrong.meng@gmail.com>
2024-12-22 23:14:56 +0800

    Expand copy_to arg list to avoid passing duplicated arguments

--- R/dbplyr-src.R
+++ R/dbplyr-src.R
@@ -174,24 +174,37 @@ tbl.PrestoConnection <- function(src, from, ...) {
 #' @param with An optional WITH clause for the CREATE TABLE statement.
 #' @rdname dplyr_source_function_implementations
 #' @keywords internal
-copy_to.src_presto <- function(dest, df, name = deparse(substitute(df)), overwrite = FALSE,
+copy_to.src_presto <- function(dest, df, name = deparse(substitute(df)),
+                               overwrite = FALSE,
+                               types = NULL,
+                               temporary = FALSE,
+                               unique_indexes = NULL,
+                               indexes = NULL,
+                               analyze = FALSE,
                                ...,
+                               in_transaction = FALSE,
                                with = NULL) {
   if (inherits(df, "tbl_sql") && dplyr::same_src(df$src, dest)) {
-    out <- dplyr::compute(df,
+    out <- dplyr::compute(
+      df,
       name = name,
-      temporary = FALSE,
-      analyze = FALSE,
+      temporary = temporary,
+      unique_indexes = unique_indexes,
+      indexes = indexes,
+      analyze = analyze,
       ...
     )
   } else {
     df <- as.data.frame(dplyr::collect(df))
-    name <- dbplyr::db_copy_to(con = dest$con, table = name, values = df,
+    name <- dbplyr::db_copy_to(
+      con = dest$con, table = name, values = df,
       overwrite = overwrite,
-      types = NULL,
-      temporary = FALSE,
-      analyze = FALSE,
-      in_transaction = FALSE,
+      types = types,
+      temporary = temporary,
+      unique_indexes = unique_indexes,
+      indexes = indexes,
+      analyze = analyze,
+      in_transaction = in_transaction,
       with = with,
       ...
     )
@@ -232,13 +245,29 @@ collect.tbl_presto <- function(x, ..., n = Inf, warn_incomplete = TRUE) {
 #' @inheritParams dplyr::copy_to
 #' @rdname dplyr_source_function_implementations
 #' @keywords internal
-copy_to.PrestoConnection <- function(dest, df, name = deparse(substitute(df)), overwrite = FALSE, ..., with = NULL) {
+copy_to.PrestoConnection <- function(
+    dest, df, name = deparse(substitute(df)),
+    overwrite = FALSE,
+    types = NULL,
+    temporary = FALSE,
+    unique_indexes = NULL,
+    indexes = NULL,
+    analyze = FALSE,
+    ...,
+    in_transaction = FALSE,
+    with = NULL) {
   copy_to(
     dest = src_presto(con = dest),
     df = df,
     name = name,
     overwrite = overwrite,
+    types = types,
+    temporary = temporary,
+    unique_indexes = unique_indexes,
+    indexes = indexes,
+    analyze = analyze,
     ...,
+    in_transaction = in_transaction,
     with = with
   )
 }
