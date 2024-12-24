e29e05f3735575bc4e33939e5e25763598fedffe
Jarod G.R. Meng <genrong.meng@gmail.com>
2024-12-24 22:01:04 +0800

    Add dbQuoteIdentifier support for dbplyr_table_path

--- R/dbQuoteIdentifier.R
+++ R/dbQuoteIdentifier.R
@@ -15,7 +15,7 @@ NULL
     schema = as.character(x$schema),
     table = as.character(x$table)
   )
-  DBI::dbQuoteIdentifier(conn, x_id)
+  DBI::dbQuoteIdentifier(conn, x_id, ...)
 }
 
 setOldClass("dbplyr_schema")
@@ -27,4 +27,59 @@ setMethod(
   "dbQuoteIdentifier",
   signature("PrestoConnection", "dbplyr_schema"),
   .dbQuoteIdentifier_PrestoConnection_dbplyr_schema
+)
+
+#' @rdname PrestoConnection-class
+#' @inheritParams DBI::dbQuoteIdentifier
+#' @usage NULL
+.dbQuoteIdentifier_PrestoConnection_dbplyr_table_path <- function(conn, x, ...) {
+  x_components <- dbplyr::table_path_components(x, conn)[[1]]
+  if (length(x_components) == 3L) {
+    if (x_components[1] != conn@catalog) {
+      stop("Mismatched catalogs", call. = FALSE)
+    }
+    x_components <- x_components[2L:3L]
+  }
+  if (length(x_components) == 2L) {
+    x_id <- DBI::Id(
+      schema = as.character(x_components[1]),
+      table = as.character(x_components[2])
+    )
+    x_qid <- DBI::dbQuoteIdentifier(conn, x_id, ...)
+  }
+  if (length(x_components) == 1L) {
+    x_qid <- DBI::dbQuoteIdentifier(conn, x_components, ...)
+  }
+  return(x_qid)
+}
+
+setOldClass("dbplyr_table_path")
+
+#' @rdname PrestoConnection-class
+#' @importMethodsFrom DBI dbQuoteIdentifier
+#' @export
+setMethod(
+  "dbQuoteIdentifier",
+  signature("PrestoConnection", "dbplyr_table_path"),
+  .dbQuoteIdentifier_PrestoConnection_dbplyr_table_path
+)
+
+#' @rdname PrestoConnection-class
+#' @inheritParams DBI::dbQuoteIdentifier
+#' @usage NULL
+.dbQuoteIdentifier_PrestoConnection_AsIs <- function(conn, x, ...) {
+  if (utils::packageVersion("dbplyr") < "2.5.0") {
+    stop("Using I() in table name required dbplyr >= 2.5.0", call. = FALSE)
+  }
+  x_tp <- dbplyr::as_table_path(x, conn)
+  dbQuoteIdentifier(conn, x_tp, ...)
+}
+
+#' @rdname PrestoConnection-class
+#' @importMethodsFrom DBI dbQuoteIdentifier
+#' @export
+setMethod(
+  "dbQuoteIdentifier",
+  signature("PrestoConnection", "AsIs"),
+  .dbQuoteIdentifier_PrestoConnection_AsIs
 )
